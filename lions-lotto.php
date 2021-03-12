890320eaa158f57445d70e2f7f1535c3be7c9543
David Woo <david@familywoo.com>
2021-03-12 17:46:02 +0000

    committing first php file

---
+++ lions-lotto.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: lions-lotto
+Plugin URI: https://southamlions.org.uk
+Description: Provides Simple Lottery functionality.
+Version: 0.1
+Author: david woo
+Author URI: https://southamlions.org.uk
+License: MIT
+Text Domain: lions-lotto
+*/
+
